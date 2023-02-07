bc99ae3d03c4e53d45e9d1a235a213360bd2b399
该起床啦 <ss56163228@163.com>
2023-02-07 23:23:21 +0800

    first commit

---
+++ src/main/java/com/huawei/GitTest.java
@@ -0,0 +1,12 @@
+package com.huawei;
+
+/**
+ * @DESCRIPTION:
+ * @PACKAGE_NAME:QinRui
+ * @CREATE_DATE:2023/2/7_星期二_23:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
