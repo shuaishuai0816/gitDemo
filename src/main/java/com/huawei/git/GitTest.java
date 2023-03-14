5fec84172dfaceb0cb8f1782f9ff70c3ec1cc4ed
该起床啦 <ss56163228@163.com>
2023-03-14 22:12:53 +0800

    revert commit again

--- src/main/java/com/huawei/git/GitTest.java
+++ src/main/java/com/huawei/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hot-fix git");
         System.out.println("revert commit");
+        System.out.println("revert commit again");
     }
 }
